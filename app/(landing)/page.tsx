da655a62c3bb3f187f6b6d2f94994f2baffaa049
gulifan <gulifan@dxy.cn>
2024-02-13 16:35:10 +0800

    feat: landing 页面增加登录和注册跳转

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,12 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 const LandingPage = () => {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
       landing page (unprotected route)
-      <Button variant="destructive">Click</Button>;
+      <UserButton afterSignOutUrl="/" />
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
     </div>
   );
 };
